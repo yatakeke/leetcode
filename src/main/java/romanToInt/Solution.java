fb9dadeff90019756f34beea434550096b0dde9b
yatakeke <yatasunshine.1456@gmail.com>
2024-04-12 07:49:23 +0900

    refactoring by hashmap

--- src/main/java/romanToInt/Solution.java
+++ src/main/java/romanToInt/Solution.java
@@ -1,19 +1,20 @@
 package romanToInt;
 
+import java.util.HashMap;
+
 public class Solution {
     public int romanToInt(String s) {
 
+        var countMap = new HashMap<String, Integer>();
+        countMap.put("I", 1);
+        countMap.put("X", 10);
+        countMap.put("C", 100);
+
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (String.valueOf(s.charAt(i)).equals("I")) {
-                result++;
-            }
-            if (String.valueOf(s.charAt(i)).equals("X")) {
-                result += 10;
-            }
-            if (String.valueOf(s.charAt(i)).equals("C")) {
-                result += 100;
-            }
+            var chatAt = String.valueOf(s.charAt(i));
+            result += countMap.get(chatAt);
+
         }
         return result;
     }
