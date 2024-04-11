25e6795ad69801ab15260671fab8b769b4ffe619
yatakeke <yatasunshine.1456@gmail.com>
2024-04-12 07:43:27 +0900

    introduced parameterized test

--- src/test/java/romanToInt/SolutionTest.java
+++ src/test/java/romanToInt/SolutionTest.java
@@ -1,44 +1,25 @@
 package romanToInt;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
-    @Test
-    public void Iを渡すと1を返す() {
-
-        var s = "I";
+    @ParameterizedTest
+    @CsvSource({
+            "I,1",
+            "II,2",
+            "X,10",
+            "XX,20",
+    })
+    public void ローマ数字を渡すと数字に変換される(String s, int expected){
         var solution = new Solution();
         var result = solution.romanToInt(s);
-        assertEquals(1, result);
+        assertEquals(expected, result);
     }
 
-    @Test
-    public void IIを渡すと2を返す() {
 
-        var s = "II";
-        var solution = new Solution();
-        var result = solution.romanToInt(s);
-        assertEquals(2, result);
-    }
-
-    @Test
-    public void Xを渡すと10を返す() {
-
-        var s = "X";
-        var solution = new Solution();
-        var result = solution.romanToInt(s);
-        assertEquals(10, result);
-    }
-
-    @Test
-    public void XXを渡すと20を返す() {
-
-        var s = "XX";
-        var solution = new Solution();
-        var result = solution.romanToInt(s);
-        assertEquals(20, result);
-    }
 }
