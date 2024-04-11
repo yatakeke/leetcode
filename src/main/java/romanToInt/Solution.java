ab3cd62141d2fc53bd4b2280f8a91c24288162c7
yatakeke <yatasunshine.1456@gmail.com>
2024-04-12 07:51:25 +0900

    exracted countmap as field

--- src/main/java/romanToInt/Solution.java
+++ src/main/java/romanToInt/Solution.java
@@ -3,19 +3,24 @@ package romanToInt;
 import java.util.HashMap;
 
 public class Solution {
-    public int romanToInt(String s) {
 
-        var countMap = new HashMap<String, Integer>();
-        countMap.put("I", 1);
-        countMap.put("X", 10);
-        countMap.put("C", 100);
+    private HashMap<String, Integer> countMap = getCountMap();
+    public int romanToInt(String s) {
 
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
             var chatAt = String.valueOf(s.charAt(i));
-            result += countMap.get(chatAt);
+            result += this.countMap.get(chatAt);
 
         }
         return result;
+    }
+
+    private HashMap<String, Integer> getCountMap() {
+        var countMap = new HashMap<String, Integer>();
+        countMap.put("I", 1);
+        countMap.put("X", 10);
+        countMap.put("C", 100);
+        return countMap;
     }
 }
