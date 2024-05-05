7f77a4c4d94d441731bc5272f6b21e02f7505f4a
yatakeke <yatasunshine.1456@gmail.com>
2024-05-05 11:25:14 +0900

    count string frequency

--- src/main/java/SudokuSolver/Solution.java
+++ src/main/java/SudokuSolver/Solution.java
@@ -1,8 +1,19 @@
 package SudokuSolver;
 
+import java.util.HashMap;
+
 class Solution {
     public void solveSudoku(char[][] board) {
+    }
 
-
+    public HashMap<String, Integer> countFrequency(String[][] input) {
+        var result = new HashMap<String, Integer>();
+        for (var row: input) {
+            for (var element: row) {
+                var count = result.getOrDefault(element, 0);
+                result.put(element, ++count);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/SudokuSolver/SolutionTest.java
+++ src/test/java/SudokuSolver/SolutionTest.java
@@ -2,13 +2,29 @@ package SudokuSolver;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
 
     @Test
-    public void firstTest() {
-        assertEquals(1, 0);
+    public void 数字ごとに出現頻度を計算する() {
+        String[][] input = {
+                {"5","3",".",".","7",".",".",".","."},
+                {"6",".",".","1","9","5",".",".","."},
+                {".","9","8",".",".",".",".","6","."},
+                {"8",".",".",".","6",".",".",".","3"},
+                {"4",".",".","8",".","3",".",".","1"},
+                {"7",".",".",".","2",".",".",".","6"},
+                {".","6",".",".",".",".","2","8","."},
+                {".",".",".","4","1","9",".",".","5"},
+                {".",".",".",".","8",".",".","7","9"}
+        };
+
+        var solution = new Solution();
+        HashMap<String, Integer> frequency = solution.countFrequency(input);
+        assertEquals(5, frequency.get("8"));
     }
 }
