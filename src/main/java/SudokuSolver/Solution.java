b297396c7acb413737a9dadefa8930cf475c4bf4
yatakeke <yatasunshine.1456@gmail.com>
2024-05-05 21:43:42 +0900

    座標からblock番号がわかるようにした

--- src/main/java/SudokuSolver/Solution.java
+++ src/main/java/SudokuSolver/Solution.java
@@ -1,19 +1,21 @@
 package SudokuSolver;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 class Solution {
-    public void solveSudoku(char[][] board) {
+    public char[][] solveSudoku(char[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+            var block = extractBlock(board, i);
+        }
+        return null;
     }
 
-    public HashMap<String, Integer> countFrequency(char[][] input) {
+    public HashMap<String, Integer> countFrequency(char[][] board) {
         var result = new HashMap<String, Integer>();
-        for (var row: input) {
+        for (var row: board) {
             for (var c: row) {
                 var frequency = result.getOrDefault(String.valueOf(c), 0);
                 result.put(String.valueOf(c), ++frequency);
@@ -22,24 +24,28 @@ class Solution {
         return result;
     }
 
-    public List<String> extractColumn(char[][] input, int rowNumber) {
+    public List<String> extractColumn(char[][] board, int rowNumber) {
         var result = new ArrayList<String>();
-        for (var row: input) {
+        for (var row: board) {
             result.add(String.valueOf(row[rowNumber]));
         }
         return result;
     }
 
-    public List<String> extractBlock(char[][] input, int blockNumber) {
+    public List<String> extractBlock(char[][] board, int blockNumber) {
         var result = new ArrayList<String>();
         var offsetRow = Math.floorDiv(blockNumber, 3);
         for (var i = 3 * offsetRow; i < 3 * offsetRow + 3; i++) {
-            var row = input[i];
+            var row = board[i];
             var offsetColumn = Math.floorMod(blockNumber, 3);
             for (var j = 3 * offsetColumn; j < 3 * offsetColumn + 3; j++) {
                 result.add(String.valueOf(row[j]));
             }
         }
         return result;
+    }
+
+    public int calculateBlockNumber(int i, int j) {
+        return Math.floorDiv(i, 3) * 3 + Math.floorDiv(j, 3);
     }
 }
--- src/test/java/SudokuSolver/SolutionTest.java
+++ src/test/java/SudokuSolver/SolutionTest.java
@@ -1,24 +1,26 @@
 package SudokuSolver;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
 
 
-    private char[][] input;
+    private char[][] board;
     private Solution solution;
 
     @BeforeEach
     void setUp() {
-        input = new char[][]{
+        board = new char[][]{
                 {'5','3','.','.','7','.','.','.','.'},
                 {'6','.','.','1','9','5','.','.','.'},
                 {'.','9','8','.','.','.','.','6','.'},
@@ -35,7 +37,7 @@ class SolutionTest {
     @Test
     public void 数字ごとに出現頻度を計算する() {
 
-        HashMap<String, Integer> frequency = solution.countFrequency(input);
+        HashMap<String, Integer> frequency = solution.countFrequency(board);
         assertThat(frequency.get("8")).isEqualTo(5);
         assertThat(frequency.get("9")).isEqualTo(4);
     }
@@ -43,7 +45,7 @@ class SolutionTest {
     @Test
     @DisplayName("列が抜き出せる")
     public void canExtractColumn() {
-        var column = solution.extractColumn(input, 0);
+        var column = solution.extractColumn(board, 0);
         assertThat(column).isEqualTo(
                 Arrays.asList("5", "6", ".", "8", "4", "7", ".", ".", ".")
         );
@@ -52,7 +54,7 @@ class SolutionTest {
     @Test
     @DisplayName("1つ目のブロックが抜き出せる")
     public void canExtractFirstBlock() {
-        var block = solution.extractBlock(input, 0);
+        var block = solution.extractBlock(board, 0);
         assertThat(block).isEqualTo(
                 Arrays.asList("5","3",".","6",".",".",".","9","8")
         );
@@ -60,9 +62,28 @@ class SolutionTest {
     @Test
     @DisplayName("9つ目のブロックが抜き出せる")
     public void canExtractNinthBlock() {
-        var block = solution.extractBlock(input, 8);
+        var block = solution.extractBlock(board, 8);
         assertThat(block).isEqualTo(
                 Arrays.asList("2","8",".",".",".","5",".","7","9")
         );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,0,0",
+            "2,4,1",
+            "8,8,8"
+    })
+    @DisplayName("座標からブロック番号が割り出せる")
+    public void canCalculateBlockNumber(int i, int j, int expect) {
+        assertThat(solution.calculateBlockNumber(i, j)).isEqualTo(expect);
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("1行6列目が8であることが確定できる")
+    public void test() {
+        var answer = solution.solveSudoku(board);
+        assertThat(answer[0][5]).isEqualTo("8");
     }
 }
